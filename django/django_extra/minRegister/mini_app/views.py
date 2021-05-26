7322de9bd6dfc6178628890901db763cebbdce3f
Helen WANG <wangyixuan@msn.com>
2021-05-26 10:25:18 -0700

    update some django forms exercises

--- django/django_extra/minRegister/mini_app/views.py
+++ django/django_extra/minRegister/mini_app/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render,redirect
-# from .forms import CreatUserForm
+from .forms import CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            password2= form.cleaned_data.get('password2')
-            if password!='' and password2!='':
-                request.session['new'] = True
+            first_name = form.cleaned_data.get('first_name')
+
+            # password = form.cleaned_data.get('password1')
             
-            request.session['username'] = username
+            request.session['first_name'] = first_name
             
             return redirect('/welcome')
         return render(request,'index.html',{'form':form})
@@ -32,17 +30,18 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 def login_page(request):
+    form = CreateUserForm()
     if request.method == 'POST':
         username = request.POST.get('username')
         password= request.POST.get('password1')
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request,user)
-            request.session['username'] = username
+            request.session['user_fname'] = user.first_name
             return redirect('/welcome')
         else:
             messages.error(request,"username or email not correct")
             return redirect('/')
-    form = UserCreationForm()
+    form = CreateUserForm()
     context = {'form':form}
     return render(request,'index.html', context)
